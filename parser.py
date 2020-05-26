644b7e498592192abb6fe2a95e96be2b2063d4d5
taylormccaul <31233095+taylormccaul@users.noreply.github.com>
2020-05-26 11:47:46 -0400

    Added parsing for some html char codes

--- parser.py
+++ parser.py
@@ -16,13 +16,12 @@ else:
 
 data = """"""
 
-#time.sleep(5)
-
-#print("Searching...")
-xml_data = open("rss.xml", "r")
-#xml_data = urllib.request.urlopen("https://www.reddit.com/r/" + link.lower() + "/.rss")
+print("Searching...")
+time.sleep(3)
+#xml_data = open("rss.xml", "r")
+xml_data = urllib.request.urlopen("https://www.reddit.com/r/" + link.lower() + "/.rss")
 for line in xml_data:
-    data += line.strip()#line.decode().strip()
+    data += line.decode().strip()
 
 m = re.search("<feed xmlns=\"http[s]*:\/\/\w+\W+\w+\W+\w+\/\w*\/\w*\">", data)
 if m != None:
@@ -37,44 +36,43 @@ post_counter = 0
 replace_list = ["<p>", "</p>", "<tr>", "</tr>", "<td>",
 "</td>", "<table>", "</table>", "<!-- SC_OFF -->",
 "<div class=\"md\">", "</a>", "<strong>", "</strong>",
-"<br/>"]
+"<br/>", "<h1>", "</h1>", "<ul>", "</ul>"]
 
 for entry in entries:
     print("Title:", entry.find('title').text)
     print("User:", entry.find('author')[0].text)
     print("Last updated:", entry.find('updated').text)
+    original_link = entry.find('link').attrib['href']
+    print("Original link:", original_link)
 
     content = entry.find('content').text
 
     for item in replace_list:
         content = content.replace(item, "")
 
+    #x = re.search('<link href=("https://www.reddit.com(\/r\/)+.*)+>', content)
     for link in re.finditer('<a\s+(?:[^>]*?\s+)?href=([\"\'])(.*?)\\1>', content):
-        content = content.replace(link.group(0), "")
+        if re.search('<a\s+(?:[^>]*?\s+)?href=([\"\'])https://www.reddit.com\/r\/\w+\/comments\/(.*?)>', link.group(0)) != None and link.group(0)[9:len(link.group(0)) - 2] != original_link:
+            content = content.replace(link.group(0), link.group(0)[9:len(link.group(0))-2] + " ")
+        else:
+            content = content.replace(link.group(0), '')
+        #x = re.search('<a href=("https://www.reddit.com(\/r\/)+.*)+>', content)
+        #content = content.replace(link.group(0), "")
+        for img in re.finditer('<img\s+(?:[^>]*?\s+)?src=([\"\'])(.*?)\\1 alt=([\"\'])(.*?)\\1 title=([\"\'])(.*?)\\1 \/>', content):
+            for src in re.finditer('src=([\"\'])(.*?)\\1', img.group(0)):
+                content = content.replace(img.group(0), src.group(0)[5:len(src.group(0))-1])
 
-    for img in re.finditer('<img\s+(?:[^>]*?\s+)?src=([\"\'])(.*?)\\1 alt=([\"\'])(.*?)\\1 title=([\"\'])(.*?)\\1 \/>', content):
-        for src in re.finditer('src=([\"\'])(.*?)\\1', img.group(0)):
-            content = content.replace(img.group(0), src.group(0)[5:len(src.group(0))-1])
+    content = content.replace("&amp;", "&")
+    content = content.replace("&#39;", "'")
+    content = content.replace("&quot;", "\"")
+    content = content.replace("<li>", "* ")
+    content = content.replace("</li>", "\n")
+
     split_content = content.partition('</div>')[0]
 
     if "&#32" in split_content:
-        print(split_content.partition('&#32')[0].strip())
+        print("Post:", split_content.partition('&#32')[0].strip())
     else:
-        print(split_content.strip())
-
-    for m in re.finditer(r'<span><a href=\"https://\w*.\w*.\w*/\w*.*/*\w*></span>', content):
-        for n in re.finditer(r'https://[www]*[i]*.\w+.\w+.\w+/\w+/*\w*/*\w*', m.group(0)):
-            if re.search("https://www.reddit.com.\w+/\w+/*\w*/*\w*", n.group(0)) == None and re.search("https://[(i)(media3)(imgur)(giphy)]+[www]{0}.\w+.\w+.\w+/\w+/*\w*/*\w*", m.group(0)) == None: #(re.search("https://i.\w+.\w+.\w+/\w+/*\w*/*\w*", n.group(0)) != None):
-                print('External link: %s' % (n.group(0)))
-            elif re.search("https://[(i)(media3)(imgur)(giphy)]+[www]{0}.\w+.\w+.\w+/\w+/*\w*/*\w*", m.group(0)) != None and re.search("https://www.reddit.com.\w+/\w+/*\w*/*\w*", n.group(0)) == None:
-                print('Image: %s' % (n.group(0)))
-            else:
-                post = n.group(0)
-                if post_check[post_counter] == post:
-                    print("")
-                else:
-                    print('Post: %s' % (n.group(0)))
-                    post_check.append(post)
-                    post_counter += 1
+        print("Post:", split_content.strip())
     print('\n')
     input('')
